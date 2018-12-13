3a28b558e0a73cf0bf511743ff415d8f6be658f3
Zachary Ferreira <Pearlybrah@github.com>
2018-12-12 19:56:55 -0800

    adding routes with data test

--- 16-HotRestaurant/server.js
+++ 16-HotRestaurant/server.js
@@ -9,14 +9,24 @@ app.use(express.json());
 
 
 var newGuests = [];
+var waitList = [];
 
-function Guests(name, phone, email, userID) {
-    this.name = name
-    this.phone = phone,
-    this.email = email,
-    this.userID = userID
-    newGuests.push(Guests());
+let waiters = {
+    name: "",
+    phone: "",
+    email: "",
+    userID: ""
 };
+let guest = {
+    name: "",
+    phone: "",
+    email: "",
+    userID: ""
+};
+
+
+
+
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -29,6 +39,11 @@ app.get("/tables", function(req, res) {
 
 app.get("/reserves", function(req, res) {
     res.sendFile(path.join(__dirname, "reserves.html"));
+});
+
+app.post("/api/tables", function(req, res) {
+    newGuests.push(guest);
+    waitList.push(waiters);
 });
 
 app.listen(PORT, function() {
